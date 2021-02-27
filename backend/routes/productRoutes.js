9dbbfa407ac895752b63120b6e1e5158340a9d62
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-02-27 16:45:36 +0530

    fetching data from database

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,36 @@
 import express from "express";
+import asyncHandler from "express-async-handler";
 const Router = express.Router();
 import Product from "../models/productModel.js";
+// import Product from "./"
 
 // @desc    Fetch All Products
 // @access  Public
 // @routes  /api/products
 
-Router.get("/", async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
-});
+Router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.json(products);
+  })
+);
 
 // @desc    Fetch single Product
 // @access  Public
 // @routes  /api/products/:id
 
-Router.get("/:id", async (req, res) => {
-  try {
+Router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
 
-    res.json(product);
-  } catch (err) {
-    console.log(err);
-  }
-});
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "No Product is found" });
+    }
+  })
+);
 
 export default Router;
