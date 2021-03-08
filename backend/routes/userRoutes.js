f36a8c0be450655fdbaf947fadab55aaa4882b30
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-03-09 01:36:35 +0530

    update profile endpoint added in backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 const Router = express.Router();
-import { authUser, getUserProfile, registerUser } from "../controllers/userController.js";
+import {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+} from "../controllers/userController.js";
 import { protect } from "../middlewares/authMiddleware.js";
 
 Router.post("/", registerUser);
 Router.post("/login", authUser);
-Router.get("/profile", protect, getUserProfile);
+Router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
 
 export default Router;
