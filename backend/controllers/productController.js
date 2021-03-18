213729093b58b682c646175ff3caa2dd10fb7a6e
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-03-19 00:42:01 +0530

    added create review functionality in frontend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -106,7 +106,7 @@ const createProductReview = asyncHandler(async (req, res) => {
     };
 
     product.reviews.push(review);
-    product.numReviews = product.reviews.length;
+    product.numReview = product.reviews.length;
 
     product.rating =
       product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
